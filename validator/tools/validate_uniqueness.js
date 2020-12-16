75ca4ab4e666aabdcb2b45805b60eaee809bb71a
Irene <irenemlcarron@gmail.com>
2020-12-16 17:05:53 +0100

    Fix identifier uniqueness validation (#18)

--- validator/tools/validate_uniqueness.js
+++ validator/tools/validate_uniqueness.js
@@ -9,8 +9,8 @@ function hasDifferentDomainType(entity1, entity2){
 }
 
 function hasSameConditions(entity1, entity2){
-    let e1conditions = entity1.conditions || []
-    let e2conditions = entity2.conditions || []
+    let e1conditions = entity1.synthesis.conditions || []
+    let e2conditions = entity2.synthesis.conditions || []
 
     if(isEqual(new Set(e1conditions), new Set(e2conditions))){
         return true
@@ -31,14 +31,15 @@ function hasSameConditions(entity1, entity2){
 
 const RULES = [
     {
-        name: 'Entities with the same id and conditions must have the same domain and type',
+        name: 'Entities with the same identifier and conditions must have the same domain and type',
         apply: def => {
-            if(def.identifier != undefined) {
-                if (ENTITY.has(def.identifier) && hasSameConditions(ENTITY.get(def.identifier), def) && hasDifferentDomainType(ENTITY.get(def.identifier), def)) {
-                    throw `Same entity ID criteria ${def.identifier} and conditions assigned to different domain types.`
+            if(def.synthesis != undefined) {
+                const identifier = def.synthesis.identifier
+                if (ENTITY.has(identifier) && hasSameConditions(ENTITY.get(identifier), def) && hasDifferentDomainType(ENTITY.get(identifier), def)) {
+                    throw `Same entity ID criteria ${identifier} and conditions assigned to different domain types.`
                 }
 
-                ENTITY.set(def.identifier, def)
+                ENTITY.set(identifier, def)
             }
         }
     }
@@ -51,12 +52,12 @@ RULES.forEach(rule => {
                     try {
                         rule.apply(definition)
                     } catch (errorMessage) {
-                        console.error(`Definition for ${definition.domain}-${definition.type}:${definition.identifier} violates rule "${rule.name}":`)
+                        console.error(`Definition for ${definition.domain}-${definition.type} violates rule "${rule.name}":`)
                         console.error(errorMessage)
                         // terminate early
                         process.exit(1)
                     }
-            console.log(`=> processed ${definition.domain}-${definition.type}:${definition.identifier}... valid for rule "${rule.name}"`)
+            console.log(`=> processed ${definition.domain}-${definition.type}... valid for rule "${rule.name}"`)
             }))
     .catch(err => console.log(err))
     }
