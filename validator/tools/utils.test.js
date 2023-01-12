39911607ae9744ade7f790782042840c58467155
Phillip Kuznetsov <pkuznetsov@pixielabs.ai>
2023-01-12 14:32:40 -0800

    Adding dashboardWithVariables test that ensures the sanitizer does not remove non-empty variable arrays
    Signed-off-by: Phillip Kuznetsov <pkuznetsov@pixielabs.ai>

--- validator/tools/utils.test.js
+++ validator/tools/utils.test.js
@@ -5,6 +5,7 @@ const path = require('path');
 const fs = require('fs')
 const DASHBOARDS_FOLDER = "./tools/tests/dashboards/"
 const ORIGINAL_DASHBOARD_PATH = path.resolve(DASHBOARDS_FOLDER, "rawDashboard.json" )
+const DASHBOARD_WITH_VARIABLES_PATH = path.resolve(DASHBOARDS_FOLDER, "dashboardWithVariables.json" )
 const SANITIZED_DASHBOARD_PATH = path.resolve(DASHBOARDS_FOLDER, "sanitizedDashboard.json")
 
 test('sanitizeDashboard returns the expected dashboard', () => {
@@ -19,6 +20,17 @@ test('sanitizeDashboard returns the expected dashboard', () => {
 
 test('sanitizeDashboard on an already sanitized dashboard does nothing', () => {
     let originalDashboard = fs.readFileSync(SANITIZED_DASHBOARD_PATH, FILE_ENCODING)
+
+    let sanitizedDashboard = utils.sanitizeDashboard(originalDashboard)
+
+    expect(originalDashboard).toBe(sanitizedDashboard);
+});
+
+test('sanitizeDashboard on a dashboard with variables does nothing', () => {
+    // We shouldn't remove variables if they exist, otherwise the user might erroneoully think that the dashboard is valid
+    // if they reference the variables inside of the queries. 
+    // Instead the user will hit an error when they run the validate command.
+    let originalDashboard = fs.readFileSync(DASHBOARD_WITH_VARIABLES_PATH, FILE_ENCODING)
 
     let sanitizedDashboard = utils.sanitizeDashboard(originalDashboard)
 
