4bc7688444374ee026fce0fefd70d3bbc185d84d
Otávio Carvalho <otaviocarvalho@users.noreply.github.com>
2023-07-25 13:41:58 +0200

    Rename definitions folder (#1150)
    * Rename definitions folder to entity-types
    
    * Move missing infra-awsgamelift to the new folder

--- validator/tools/props.js
+++ validator/tools/props.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DEFINITIONS_DIR: '../definitions/',
+    DEFINITIONS_DIR: '../entity-types/',
     RELATIONSHIPS_SYNTHESIS_DIR:'../relationships/synthesis/',
     DEFINITION_FILE_NAME: 'definition.yml',
     DASHBOARD_FILE_NAME_SUFFIX: 'dashboard.json',
