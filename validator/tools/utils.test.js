4cc6bf08719cd01a8a527cd7f81085f6ffbfbc5e
Adrian <a.whole.dev@gmail.com>
2023-09-01 12:24:21 +0200

    Add a Github Helper and add eslint to maintain a fixed style code. (#1209)
    * add eslint and add the github helper
    
    * Add the env values for the validate definitions
    
    * Rename the helper
    
    * Use the file encoding from the props
    
    * create an issue to send a github event
    
    * change how the validation is done
    
    * remove the test definition
    
    * Fix the validateAndRecord function
    
    * Send a notification with the request approved
    
    * change the error to catch
    
    * change the message to undefined
    
    * change the message of the load structure in the name of the synthesis rules

--- validator/tools/utils.test.js
+++ validator/tools/utils.test.js
@@ -1,38 +1,37 @@
 const utils = require('./utils');
-const { DEFINITIONS_DIR,DASHBOARD_FILE_NAME_SUFFIX,FILE_ENCODING} = require('./props');
-const {readdir} = require('fs').promises;
 const path = require('path');
-const fs = require('fs')
-const DASHBOARDS_FOLDER = "./tools/tests/dashboards/"
-const ORIGINAL_DASHBOARD_PATH = path.resolve(DASHBOARDS_FOLDER, "rawDashboard.json" )
-const DASHBOARD_WITH_VARIABLES_PATH = path.resolve(DASHBOARDS_FOLDER, "dashboardWithVariables.json" )
-const SANITIZED_DASHBOARD_PATH = path.resolve(DASHBOARDS_FOLDER, "sanitizedDashboard.json")
+const fs = require('fs');
+const { FILE_ENCODING } = require('./props');
+const DASHBOARDS_FOLDER = './tools/tests/dashboards/';
+const ORIGINAL_DASHBOARD_PATH = path.resolve(DASHBOARDS_FOLDER, 'rawDashboard.json');
+const DASHBOARD_WITH_VARIABLES_PATH = path.resolve(DASHBOARDS_FOLDER, 'dashboardWithVariables.json');
+const SANITIZED_DASHBOARD_PATH = path.resolve(DASHBOARDS_FOLDER, 'sanitizedDashboard.json');
 
 test('sanitizeDashboard returns the expected dashboard', () => {
-    let originalDashboard = fs.readFileSync(ORIGINAL_DASHBOARD_PATH, FILE_ENCODING)
-    let expectedSanitizedDashboard = fs.readFileSync(SANITIZED_DASHBOARD_PATH, FILE_ENCODING)
+  const originalDashboard = fs.readFileSync(ORIGINAL_DASHBOARD_PATH, FILE_ENCODING);
+  const expectedSanitizedDashboard = fs.readFileSync(SANITIZED_DASHBOARD_PATH, FILE_ENCODING);
 
-    let sanitizedDashboard = utils.sanitizeDashboard(originalDashboard)
+  const sanitizedDashboard = utils.sanitizeDashboard(originalDashboard);
 
-    expect(originalDashboard).not.toBe(sanitizedDashboard);
-    expect(expectedSanitizedDashboard).toBe(sanitizedDashboard);
+  expect(originalDashboard).not.toBe(sanitizedDashboard);
+  expect(expectedSanitizedDashboard).toBe(sanitizedDashboard);
 });
 
 test('sanitizeDashboard on an already sanitized dashboard does nothing', () => {
-    let originalDashboard = fs.readFileSync(SANITIZED_DASHBOARD_PATH, FILE_ENCODING)
+  const originalDashboard = fs.readFileSync(SANITIZED_DASHBOARD_PATH, FILE_ENCODING);
 
-    let sanitizedDashboard = utils.sanitizeDashboard(originalDashboard)
+  const sanitizedDashboard = utils.sanitizeDashboard(originalDashboard);
 
-    expect(originalDashboard).toBe(sanitizedDashboard);
+  expect(originalDashboard).toBe(sanitizedDashboard);
 });
 
 test('sanitizeDashboard on a dashboard with variables does nothing', () => {
-    // We shouldn't remove variables if they exist, otherwise the user might erroneoully think that the dashboard is valid
-    // if they reference the variables inside of the queries. 
-    // Instead the user will hit an error when they run the validate command.
-    let originalDashboard = fs.readFileSync(DASHBOARD_WITH_VARIABLES_PATH, FILE_ENCODING)
+  // We shouldn't remove variables if they exist, otherwise the user might erroneoully think that the dashboard is valid
+  // if they reference the variables inside of the queries.
+  // Instead the user will hit an error when they run the validate command.
+  const originalDashboard = fs.readFileSync(DASHBOARD_WITH_VARIABLES_PATH, FILE_ENCODING);
 
-    let sanitizedDashboard = utils.sanitizeDashboard(originalDashboard)
+  const sanitizedDashboard = utils.sanitizeDashboard(originalDashboard);
 
-    expect(originalDashboard).toBe(sanitizedDashboard);
+  expect(originalDashboard).toBe(sanitizedDashboard);
 });
