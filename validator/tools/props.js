aaf6d699ebbc26ca02ea6a61f2c29cb6a0257737
Joan Pujol <jpujol@newrelic.com>
2024-02-28 16:35:10 +0100

    Fix Github validation action

--- validator/tools/props.js
+++ validator/tools/props.js
@@ -1,6 +1,6 @@
 module.exports = {
-  DEFINITIONS_DIR: '../entity-types/',
-  RELATIONSHIPS_SYNTHESIS_DIR: '../relationships/synthesis/',
+  DEFINITIONS_DIR: process.env.DEFINITIONS_DIR || '../entity-types/',
+  RELATIONSHIPS_SYNTHESIS_DIR: process.env.RELATIONSHIPS_SYNTHESIS_DIR || '../relationships/synthesis/',
   DEFINITION_FILE_NAME: 'definition.yml',
   DASHBOARD_FILE_NAME_SUFFIX: 'dashboard.json',
   FILE_ENCODING: 'UTF-8'
